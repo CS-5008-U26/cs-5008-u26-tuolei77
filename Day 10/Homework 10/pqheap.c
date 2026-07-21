585d496ac137e9900a31710261239bf3301bcc2f
Deernsesame <deernsesame@Sesames-MacBook-Air.local>
2026-07-20 23:41:36 -0700

    submit hw 10

---
+++ Day 10/Homework 10/pqheap.c
@@ -0,0 +1,224 @@
+/* 
+ * Priority queue using array-based minheap.
+ *
+ * Enqueue: Place the new value at the end of the array (index pqsize),
+ * call heapifyUp to bubble it up until the minheap property is restored.
+ *
+ * Dequeue: The minimum is always at pq[0]. Move the last element into
+ * the root position, call heapifyDown to move the new root into its correct place.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_TO_PRINT 100        // max number of elements to print
+#define MAX_VALUE 100000000     // max value in randomly generated data
+
+int *pq = NULL;
+int pqsize = 0;
+int pqcapacity = 0;
+
+/* Y O U R   S O R T   F U N C T I O N */
+
+void heapifyUp(int *a, int n, int here) {
+
+    int parent = (here - 1) / 2;
+
+    // if here is not root and here is smaller than its parent, do the swap
+    if (here != 0 && a[here] < a[parent]) {
+
+        int temp = a[here];
+        a[here] = a[parent];
+        a[parent] = temp;
+        heapifyUp(a, n, parent);
+
+    }
+}
+
+void heapifyDown(int *a, int n, int here) {
+    // find left child
+	int child1 = 2 * here + 1;
+    // find right child
+    int child2 = 2 * here + 2;
+
+    // find smallest value of the three
+    // when here is not a leaf node
+    int smallest = here;
+    if (child1 < n && a[child1] < a[smallest]) smallest = child1;
+    if (child2 < n && a[child2] < a[smallest]) smallest = child2;
+
+    // swap current value with smallest if smallest is not current value
+	if (smallest != here) {
+        int temp = a[here];
+        a[here] = a[smallest];
+        a[smallest] = temp;
+        // heapify down 
+        heapifyDown(a, n, smallest);   
+    }
+}
+
+/* U T I L I T Y   F U N C T I O N S */
+
+// printarray = print an array of ints
+// a is the array, n is the number of elements
+
+void printarray(int a[], int n) {
+    int numberToPrint = ((n < MAX_TO_PRINT) ? n : MAX_TO_PRINT);
+    for (int i=0; i < numberToPrint; i++) {
+        printf ("%d ", a[i]);
+    }
+    printf ("\n");
+}
+
+// genarray - return an array of random ints on the heap
+// numberofelements is how many elements in the array
+// the array is returned
+// don't forget to free the array when you are finished using it
+
+int *genarray(int numberofelements) {
+    int *result = malloc (numberofelements * sizeof(int));
+    if (result == NULL) {
+        printf ("malloc failed");
+    } else {
+        // fill the result array with random numbers between 0 and MAX_VALUE
+        for (int i=0; i<numberofelements; i++) {
+            // we have a problem
+            // On Windows the rand() function only gives a 15-bit random number
+            // This will be between 0 and 32767
+            // So we will fix this with a crude trick
+            // We will generate two random numbers, one for the high-order bits and the other
+            // for the low-order 14 bits
+            long long int r1 = rand();
+            long long int r2 = rand();
+            long long int randomValue = (r1 << 14) + (r2 & 0x3fff);
+            result[i] = (int)(randomValue % MAX_VALUE);
+        }
+    }   
+    return (result);
+}
+
+// ensure the capacity
+void pqensurecapacity(int needed) {
+    int newcapacity;
+    if (needed > pqcapacity) {
+        newcapacity = needed;
+
+        int *newpq = malloc(newcapacity * sizeof(int));
+        if (newpq == NULL) {
+            printf ("malloc failed\n");
+            return;
+        }
+        for (int i = 0; i < pqsize; i++) {
+            newpq[i] = pq[i];
+        }
+        free(pq);
+        pq = newpq;
+        pqcapacity = newcapacity;
+    }
+}
+
+// enqueue: insert n random numbers one at a time, using heapifyUp on each
+void pqenqueue(int n) {
+    if (n == 0) return;
+    // make sure there is enough room for the new numbers
+    pqensurecapacity(pqsize + n);
+    if (pqcapacity < pqsize + n) {
+        return;
+    }
+    int *temp = genarray(n);
+    if (temp == NULL) {
+        printf ("malloc failed\n");
+        return;
+    }
+    // insert each number at the end of the heap, then heapifyUp
+    for (int i = 0; i < n; i++) {
+        pq[pqsize] = temp[i];
+        pqsize++;
+        heapifyUp(pq, pqsize, pqsize - 1);
+    }
+    free(temp);
+}
+
+// dequeue: remove and print up to n numbers, one at a time
+void pqdequeue(int n) {
+    // don't try to remove more numbers than we have
+    if (n > pqsize) {
+        n = pqsize;
+    }
+    for (int i = 0; i < n; i++) {
+        printf ("%d ", pq[0]);
+        // move the last element to root, heapifyDown to correct position
+        pq[0] = pq[pqsize - 1];
+        pqsize -= 1;
+        heapifyDown(pq, pqsize, 0);
+    }
+
+    printf ("\n");
+    return;
+}
+
+// timedsort - runs a sort and records the elapsed time
+// n is how many elements
+
+void timedsort (int n) {
+    clock_t startTime = clock();        // get the start time
+    pqenqueue(n);
+    clock_t endTime = clock();          // get the end time
+    double elapsedTime = (double)(endTime - startTime) / CLOCKS_PER_SEC;
+    printf ("Elapsed time: %f seconds\n\n", elapsedTime);   // print the elapsed time
+}
+
+// timedsort - runs a sort and records the elapsed time
+// n is how many elements
+
+void timeddequeue(int n) {
+    clock_t startTime = clock();        // get the start time
+    pqdequeue(n);
+    clock_t endTime = clock();          // get the end time
+    double elapsedTime = (double)(endTime - startTime) / CLOCKS_PER_SEC;
+    printf ("Elapsed time: %f seconds\n\n", elapsedTime);   // print the elapsed time
+}
+
+/* M A I N   F U N C T I O N */
+void main () {
+    char buffer[100];
+    int nelements;
+
+    srand(time(NULL));  // seed the random number generator
+
+    // run timed sorts
+    int keepgoing = 1;
+    while (keepgoing) {
+
+        printf ("How many numbers to add: ");
+        fgets(buffer, 100, stdin);
+        if (buffer[0] == '\n') {
+            break;
+        } else {
+            nelements = atoi(buffer);
+            if (nelements < 0) {
+                printf ("Must be a nonnegative number of numbers\n");
+            } else {
+                // enqueue
+                timedsort(nelements);
+            }
+        }
+
+        printf ("How many numbers to remove: ");
+        fgets(buffer, 100, stdin);
+        if (buffer[0] == '\n') {
+            break;
+        } else {
+            nelements = atoi(buffer);
+            if (nelements < 0) {
+                printf ("Must be a nonnegative number of numbers\n");
+            } else {
+                // deque
+                timeddequeue(nelements);
+            }
+        }
+    }
+    free(pq);
+}
